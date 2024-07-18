681cff980d682b6130f67fec7e9067818dbb30a9
Lentscode <antonio.lentini2004@gmail.com>
2024-07-18 18:04:11 +0200

    Update

--- lib/config/set_up.dart
+++ lib/config/set_up.dart
@@ -4,7 +4,6 @@ import "package:path_provider/path_provider.dart";
 
 import "../data/note.dart";
 import "../data/tag.dart";
-import "../logic/logic.dart";
 
 // Creazione di un'istanza singleton di GetIt, un service locator per gestire le dipendenze.
 final getIt = GetIt.instance;
@@ -19,16 +18,4 @@ Future<void> setUp() async {
     [NoteSchema, TagSchema], // Gli schemi delle collezioni Note e Tag.
     directory: dir.path, // Il percorso della directory in cui salvare il database.
   );
-
-  // Crea un'istanza di NoteRepo, passando l'istanza di Isar e la collezione di note.
-  final noteRepo = NoteRepo(isar, isar.notes);
-
-  // Crea un'istanza di TagRepo, passando l'istanza di Isar e la collezione di tag.
-  final tagRepo = TagRepo(isar, isar.tags);
-
-  // Registra l'istanza di NoteRepo nel service locator GetIt come singleton.
-  getIt.registerSingleton<NoteRepo>(noteRepo);
-
-  // Registra l'istanza di TagRepo nel service locator GetIt come singleton.
-  getIt.registerSingleton<TagRepo>(tagRepo);
 }
--- lib/logic/tag_repo.dart
+++
@@ -1,6 +0,0 @@
-part of "logic.dart";
-
-// Implementazione della Repo per la collezione delle tags.
-class TagRepo extends Repo<Tag> {
-  const TagRepo(super.isar, super.collection);
-}
