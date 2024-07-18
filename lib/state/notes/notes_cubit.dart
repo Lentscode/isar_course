e7637ee5f7a19d9b188a8efc9a5a0efa2e0e89d1
Lentscode <antonio.lentini2004@gmail.com>
2024-07-18 18:03:25 +0200

    Update

--- lib/state/notes/notes_cubit.dart
+++
@@ -1,7 +0,0 @@
-import "../../data/note.dart";
-import "../collection/collection_cubit.dart";
-
-// Implementazione del [CollectionCubit] per le note.
-class NotesCubit extends CollectionCubit<Note> {
-  NotesCubit(super.repo);
-}
